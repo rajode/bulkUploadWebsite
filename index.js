b9f24fe28afc521a343b9dc254c9e8670f7c7e42
rajode <rgmittal5@gmail.com>
2023-10-18 17:43:52 -0600

    first commit
    skeletondone

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static('public'));
+
+// Define routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/privacy', (req, res) => {
+    res.sendFile(__dirname + '/public/privacy.html');
+});
+
+app.get('/tos', (req, res) => {
+    res.sendFile(__dirname + '/public/tos.html');
+});
+
+// Start server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
